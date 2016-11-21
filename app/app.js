3e8a6d85bba91b38a0e65feab097346e473993b8
Veeru <veeru.kavula@gmail.com>
2016-11-21 10:04:48 +0530

    added changes

--- app/app.js
+++ app/app.js
@@ -1,18 +1,19 @@
-(function(){
-    
-    angular.module("blogFest",["header"]);
-    
-     angular.module("blogFest")
+(function () {
+
+    angular.module("blogFest", ["header"]);
+
+    angular.module("blogFest")
         .config(function () {
             console.log("i am the blogFest module");
         });
-    
-    
+
+
     angular.module("blogFest")
-    .run(function(){
-        console.log("i am the blogFest run function");
-        
-    })
+        .run(function () {
+            console.log("i am the blogFest run function");
+
+        })
     
-    
+
+
 })();
